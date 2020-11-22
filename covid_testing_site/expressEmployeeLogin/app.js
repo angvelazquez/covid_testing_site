717a6b80d79ab42d5f7a2b979727a6bd0475bd50
Matthew Li-Ah-Kim <lightstripe123@gmail.com>
2020-11-22 15:55:59 -0500

    MariaDB/MySQL querying now possible through express, used env file to protect privacy

--- covid_testing_site/expressEmployeeLogin/app.js
+++ covid_testing_site/expressEmployeeLogin/app.js
@@ -4,12 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+var mariadb = require('mariadb');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var employeeLogin = require("./routes/employeeLogin");
 
 var app = express();
+var table = 'Employee';
+
+var pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_DB,
+  port: process.env.DB_PORT,
+  connectionLimit:5
+});
+
+pool.getConnection();
+
+console.log("hmmm");
+//pool.query(`select * from ${table}`)
+//    .then(res => {
+//      console.log(res); 
+//    });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,8 +43,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/blah', usersRouter);
 app.use('/employeeLogin', employeeLogin);
+app.use('/employeeHome', (req, res) => {
+  
+  pool.query(`select * from ${table}`)
+    .then(blah => {
+      console.log(blah);
+      res.send(blah); 
+    });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- covid_testing_site/src/index.js
+++ covid_testing_site/src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import EmployeeLoginApp from './employeeLogin/EmployeeLoginApp';
+import EmployeeHome from './employeeLogin/EmployeeHome';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <EmployeeLoginApp /> //I changed this to my login page, gotta separate later
+    <EmployeeHome /> 
   </React.StrictMode>,
   document.getElementById('root')
-);
+);//I changed this to my EmployeeHome page, gotta add React-Routing later to choose the page.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
